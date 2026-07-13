ebeb64652c3ec53acf4481da03ed7197f0d0dc54
BYUSAIZE <prospersaize@gmail.com>
2026-07-13 11:18:47 +0200

    Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // 1/1
+        Fraction first = new Fraction();
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+
+        Console.WriteLine();
+
+        // 5/1
+        Fraction second = new Fraction(5);
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+
+        Console.WriteLine();
+
+        // 3/4
+        Fraction third = new Fraction(3, 4);
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
+
+        Console.WriteLine();
+
+        // Using setters
+        Fraction fourth = new Fraction();
+        fourth.SetTop(1);
+        fourth.SetBottom(3);
+
+        Console.WriteLine(fourth.GetFractionString());
+        Console.WriteLine(fourth.GetDecimalValue());
     }
 }
